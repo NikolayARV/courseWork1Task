a4cdb2ef710bae96831e87012ecbdc04992498d5
razenkov <razenkov.rna@mail.ru>
2022-11-05 18:27:17 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4};
-        int arr1[] = new int[4];
-        int j =0;
-        for (int i = arr.length -1; i >= 0; i--) {
-            arr1[j] = arr[i];
-            System.out.println(arr1[j]);
-            j++;
 
-
-        }
 
 
     }
